16b2fec3d3af94c16ac6742a50b0d7dccda60e87
Rick Lupton <mail@ricklupton.name>
2026-07-10 17:24:49 +0100

    Expose `merge_activities`

--- src/flowprog/__init__.py
+++ src/flowprog/__init__.py
@@ -1,9 +1,10 @@
 from .model_structure import Process, Object, ModelStructure
 from .model_builder import ModelBuilder
+from .activities import AdditionalActivity, merge_activities
 from .backends.sympy import SympyModel
 
 try:
     from .backends.numpyro import NumpyroModel
-    __all__ = ["Process", "Object", "ModelStructure", "ModelBuilder", "SympyModel", "NumpyroModel"]
+    __all__ = ["Process", "Object", "ModelStructure", "ModelBuilder", "AdditionalActivity", "merge_activities", "SympyModel", "NumpyroModel"]
 except ImportError:
-    __all__ = ["Process", "Object", "ModelStructure", "ModelBuilder", "SympyModel"]
+    __all__ = ["Process", "Object", "ModelStructure", "ModelBuilder", "AdditionalActivity", "merge_activities", "SympyModel"]
